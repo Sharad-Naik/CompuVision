441676fdc3a831cb069d93dffb0cc18ba619f2c4
Sharad-Naik <sharad33404@gmail.com>
2022-02-16 15:14:30 +0530

    filters added in util, code restructuring

--- ImageConvolutionProject/image.py
+++ ImageConvolutionProject/image.py
@@ -3,17 +3,90 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.util import show_image_matplotlib
+from utils.util import laplacian_filter, sobel_filter, prewitt_filter, \
+    conv_image, gausian_filter, prewitt_h_filter, sobel_h_filter, image_filter
+
 
 img1 = cv2.imread(r"ImageConvolutionProject/images/color.jpg")
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
+
 img2 = cv2.imread(r"ImageConvolutionProject/images/blk_1.jpg")
+
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+
 img3 = cv2.imread(r"ImageConvolutionProject/images/blk_2.jpg")
 
-img4 = cv2.imread(r"ImageConvolutionProject/images/color.jpg")
-img5 = cv2.imread(r"ImageConvolutionProject/images/blk_1.jpg")
-img6 = cv2.imread(r"ImageConvolutionProject/images/blk_2.jpg")
+img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+img3 = cv2.cvtColor(img3, cv2.COLOR_RGB2GRAY)
 
 
-show_image_matplotlib([img1, img2, img3], 3)
-show_image_matplotlib([img4, img5, img6], 2)
 
+
+
+"""
+Filter for edge detection
+"""
+#sobel filter to detect vertical changes on image
+filter1 = sobel_filter()
+
+#laplacian filter for detecting different brightness on images
+filter2 = laplacian_filter()
+
+#prewitt filter to detect vertical changes on image
+filter3 = prewitt_filter()
+
+filter4 = gausian_filter()
+
+filter5 = prewitt_h_filter()
+
+filter6 = sobel_h_filter()
+
+filters = []
+
+"""
+convolution starts here
+"""   
+output_img = conv_image(img1, filters)
+
+print(img1.shape)
+img2 = image_filter(img2, filter2)
+img2 = image_filter(img2, filter4)
+img2 = image_filter(img2, filter2)
+
+img_length = len(output_img)
+
+
+if img_length <=3:
+    img_length = 4
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0)   
+
+        
+plt.imshow(img2, cmap=plt.get_cmap("gray"))
+
+# figure, ax = plt.subplots(nrows=int(img_length/2), ncols=int(img_length/2))
+
+# x = 0;
+# for i in range(int(img_length/2)):
+#     if x == len(filters):
+#         break
+#     for j in range(int(img_length/2)):
+#         print(i, j)
+#         ax[i, j].imshow(output_img[x], cmap=plt.get_cmap("gray"))
+#         ax[i, j].set_title("Color")
+#         x = x + 1;
+       
+# for i in range(int(img_length/2)):
+#     for j in range(int(img_length/2)):
+#         ax[i, j].axis("off")
+        
+# plt.imshow(oimg2, cmap=plt.get_cmap("gray"))
+
+# plt.tight_layout()
+
+# figure.canvas.set_window_title("Color and gray images")
+
+plt.show()
--- utils/util.py
+++ utils/util.py
@@ -4,33 +4,109 @@ Created on Sun Feb  6 19:27:16 2022
 
 @author: Sharad
 """
-import cv2
-import matplotlib.pyplot as plt
+
 import numpy as np
+from tqdm import tqdm
 
-def show_image_matplotlib(imgs, shape_d):
-    plt.rcParams['figure.figsize'] = (10.0, 10.0)
+def sobel_filter():
+    filter1 = np.array([[1, 0, -1],
+                    [2, 0, -2],
+                    [1, 0, -1]])
+    
+    return filter1
 
-    if len(imgs) < 9:
-        figure, ax = plt.subplots(nrows=3, ncols=3)
-        cntr = 0;
-        print(len(imgs))
-        if shape_d == 3:
-            for i in range(3):
-                for j in range(3):
-                    try:
-                        ax[i, j].imshow(cv2.cvtColor(imgs[cntr], cv2.COLOR_BGR2RGB))
-                        if cntr < len(imgs):
-                                cntr = cntr+1
-                    except:
-                        pass
-                            
-        if shape_d == 2:
-            for i in range(3):
-                for j in range(3):
-                    try:
-                        ax[i, j].imshow(cv2.cvtColor(imgs[cntr], cv2.COLOR_BGR2GRAY))
-                        if cntr < len(imgs):
-                                cntr = cntr+1
-                    except:
-                        pass
+
+def laplacian_filter():
+    filter1 = np.array([[0, 1, 0],
+                    [1, -4, 1],
+                    [0, 1, 0]])
+    
+    return filter1
+
+
+def prewitt_filter():
+    filter1 = np.array([[1, 0, -1],
+                    [1, 0, -1],
+                    [1, 0, -1]])    
+    
+    return filter1
+
+def sobel_h_filter():
+    filter1 = np.array([[1, 2, 1],
+                    [0, 0, 0],
+                    [-1, -1, -1]])    
+    
+    return filter1
+
+def prewitt_h_filter():
+    filter1 = np.array([[1, 1, 1],
+                    [0, 0, 0],
+                    [-1, -1, -1]])    
+    
+    return filter1
+
+def gausian_filter():
+    filter1 = (1 / 16) * np.array([[1, 2, 1],
+                                [2, 4, 2],
+                                [1, 2, 1]])    
+    
+    return filter1
+
+def conv_image(img, filters):
+    l = len(filters)
+    
+    """
+    preparing hyperparameters for convolution
+    filter size = 3
+    stride (step) = 1
+    pad = 1
+    
+    height_out = (height_in - f_size + 2 * pad)/step + 1
+    
+    """
+    img = np.pad(img, (1, 1), mode='constant', constant_values=0)
+    
+    output_img = np.zeros(tuple([l])+img.shape)
+
+    print(output_img.shape)
+    
+    
+    """
+    convolution starts here
+    """   
+    for i in tqdm(range((img.shape[0])-2)):
+        for j in range((img.shape[1]-2)):
+            patch = img[i:i+3, j:j+3]
+            
+            for k in range(l):
+                output_img[k, i, j] = np.sum(patch*filters[k])
+
+            
+    """
+    end of convolution
+    """
+    
+    #exclude values that are less than 0
+    
+    output_img = np.clip(output_img, 0, 255)
+    
+    return output_img
+
+
+def image_filter(img, filters):
+    #print("img",img.shape)
+    img = np.pad(img, (1, 1), mode='constant', constant_values=0)
+    
+    #print("pad img",img.shape)
+    output_img = np.zeros(img.shape)
+    
+    
+    for i in tqdm(range((img.shape[0])-2)):
+        for j in range((img.shape[1]-2)):
+            patch = img[i:i+3, j:j+3]
+            
+            output_img[i, j] = np.sum(patch*filters)
+            
+    output_img = np.clip(output_img, 0, 255)
+            
+    return output_img
